16cd7ce0d9f49a763607b1799e34e1c14bb1f5df
Ashu Prakash <aspksh@gmail.com>
2026-07-28 14:15:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ import av
 import cv2
 import torch
 import numpy as np
-import mediapipe as mp
+
 
 from PIL import Image
 from torchvision import transforms
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
-from model import EmotionCNN
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
+from model import EmotionCNN
 
 # ---------------- Device ----------------
 
@@ -55,78 +58,69 @@ mp_face = mp.solutions.face_detection
 
 class EmotionProcessor(VideoProcessorBase):
 
-    def __init__(self):
-
-        self.face_detector = mp_face.FaceDetection(
-            model_selection=0,
-            min_detection_confidence=0.5
-        )
-
     def recv(self, frame):
 
         img = frame.to_ndarray(format="bgr24")
 
-        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        results = self.face_detector.process(rgb)
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(80,80)
+        )
 
-        if results.detections:
+        for (x,y,w,h) in faces:
 
-            h, w, _ = img.shape
+            # thoda margin add karte hain
+            pad = 15
 
-            for detection in results.detections:
+            x1 = max(0, x-pad)
+            y1 = max(0, y-pad)
 
-                box = detection.location_data.relative_bounding_box
+            x2 = min(img.shape[1], x+w+pad)
+            y2 = min(img.shape[0], y+h+pad)
 
-                x = int(box.xmin * w)
-                y = int(box.ymin * h)
+            face = img[y1:y2, x1:x2]
 
-                bw = int(box.width * w)
-                bh = int(box.height * h)
+            if face.size == 0:
+                continue
 
-                x = max(0, x)
-                y = max(0, y)
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 
-                face = rgb[y:y+bh, x:x+bw]
+            face = Image.fromarray(face)
 
-                if face.size == 0:
-                    continue
+            tensor = transform(face).unsqueeze(0).to(device)
 
-                face = Image.fromarray(face)
+            with torch.no_grad():
 
-                tensor = transform(face).unsqueeze(0).to(device)
+                output = model(tensor)
 
-                with torch.no_grad():
+                probs = torch.softmax(output, dim=1)
 
-                    output = model(tensor)
+                confidence, pred = torch.max(probs, dim=1)
 
-                    probs = torch.softmax(output,1)
+            emotion = EMOTIONS[pred.item()]
+            confidence = confidence.item()*100
 
-                    conf, pred = torch.max(probs,1)
+            cv2.rectangle(
+                img,
+                (x1,y1),
+                (x2,y2),
+                (0,255,0),
+                2
+            )
 
-                emotion = EMOTIONS[pred.item()]
-                confidence = conf.item()*100
-
-                # Green Box
-                cv2.rectangle(
-                    img,
-                    (x,y),
-                    (x+bw,y+bh),
-                    (0,255,0),
-                    2
-                )
-
-                text = f"{emotion} {confidence:.1f}%"
-
-                cv2.putText(
-                    img,
-                    text,
-                    (x,y-10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7,
-                    (0,255,0),
-                    2
-                )
+            cv2.putText(
+                img,
+                f"{emotion} {confidence:.1f}%",
+                (x1, y1-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                (0,255,0),
+                2
+            )
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
